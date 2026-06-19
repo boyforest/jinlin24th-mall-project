a73052be1e3a280c40cce90331eac6000493af43
boyforest <3448472783@qq.com>
2026-06-19 19:54:11 +0800

    fix: AdminPasswordDTO 忽略前端 confirmPassword 字段

--- src/main/java/com/jinlin24th/jinlin/pojo/dto/AdminPasswordDTO.java
+++ src/main/java/com/jinlin24th/jinlin/pojo/dto/AdminPasswordDTO.java
@@ -1,5 +1,6 @@
 package com.jinlin24th.jinlin.pojo.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -10,6 +11,7 @@ import java.io.Serializable;
  * 用于首次登录强制改密场景及后台自主改密。
  */
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AdminPasswordDTO implements Serializable {
     private String oldPassword;
     private String newPassword;
